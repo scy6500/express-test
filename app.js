718288103e5369f114e9bafc6389e5ebb84bbaac
changyeop1 <changyeop@comcom.ai>
2023-05-21 13:41:38 +0900

    fix: change router

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.set("port", process.env.PORT || 3000);
 const { swaggerUi, specs } = require("./swagger/swagger");
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
 
-app.use("/add", addRouter);
+app.use("", addRouter);
 
 app.listen(app.get("port"), () => {
   console.log(app.get("port"), "번 포트에서 대기 중");
