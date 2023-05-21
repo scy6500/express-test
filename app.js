b36d87cadcf72cee50b410e2741dd07a3f69109e
changyeop1 <changyeop@comcom.ai>
2023-05-21 13:43:00 +0900

    feat: add substract router

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const addRouter = require("./routes/add");
+const substractRouter = require("./routes/subtract");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -10,6 +11,7 @@ const { swaggerUi, specs } = require("./swagger/swagger");
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
 
 app.use("", addRouter);
+app.use("", substractRouter);
 
 app.listen(app.get("port"), () => {
   console.log(app.get("port"), "번 포트에서 대기 중");
